b763ad12292b12d39ddb908b62b06d6afedcdbae
Shinogasa <cuulbulu@gmail.com>
2018-04-09 22:41:37 +0900

    first commit

---
+++ src/user.ts
@@ -0,0 +1,10 @@
+/**
+ * ユーザークラスです。
+ */
+export class User {
+    /**
+     * ID, 名前, 年齢をプロパティとして持ちます。
+     */
+    constructor(public id: number, public name: string, public age: number) {
+    }
+}
